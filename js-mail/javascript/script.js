f9d58df39511f056d86baa78fcfd6558714ed7f7
MatteoGiuntella <matteo.giuntella@virgilio.it>
2023-12-06 17:52:33 +0100

    esercizio email

--- js-mail/javascript/script.js
+++ js-mail/javascript/script.js
@@ -7,3 +7,42 @@
 // -1 creare una lista specifica di email che possono accedere
 // -2 creare un form dove inserie l' email
 // -3 convalidare o non l'email inserita
+
+
+let listaEmail = [
+    "matte@gmail.it",
+    "alessio@gmail.it",
+    "francesco@virgilio.it",
+]
+console.log(listaEmail)
+
+let submit = document.getElementById("submit");
+submit.addEventListener('click', function(){
+
+    let email = document.getElementById("email").value;
+    console.log(email)
+
+    let controll = false
+    console.log('controll', controll, typeof controll)
+
+    for (let i = 0; i < listaEmail.length; i++) {
+
+
+        if ( email == listaEmail[i] ) {
+            
+            controll = true;
+        }
+    }
+
+    if ( controll == true ){
+
+        alert('email valida')
+
+    }
+    else{
+
+        alert('email non valida')
+    }
+})
+
+
