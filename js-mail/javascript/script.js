de9fd7c08120df28dfd9f755ff29a0942a852041
MatteoGiuntella <matteo.giuntella@virgilio.it>
2023-12-06 16:36:39 +0100

    inizio esercizio email

---
+++ js-mail/javascript/script.js
@@ -0,0 +1,9 @@
+// Mail
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull'esito del controllo.
+
+
+// -1 creare una lista specifica di email che possono accedere
+// -2 creare un form dove inserie l' email
+// -3 convalidare o non l'email inserita
